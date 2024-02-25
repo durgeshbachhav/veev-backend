dc1911dea3072a92579215be1c3f5cca548f3359
unknown <durgeshbachhav123@gmail.com>
2024-02-25 18:12:53 +0530

    create a schemas of veev

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const subscribeSchema = mongoose.Schema({
+     subscriber: {
+          type: Schema.Types.ObjectId, // one who is subscribing
+          ref: "User"
+     },
+     channel: {
+          type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
+          ref: "User"
+     }
+}, { timestamps: true })
+
+
+const Subscribe = mongoose.model('Subscribe', subscribeSchema);
