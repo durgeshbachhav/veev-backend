a29aa8490cd4609754a0505ab6a07fe9474a1fd9
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-02-26 19:33:21 +0530

    adding a base of every controller we use  in our application

---
+++ src/controllers/healthCheck.controller.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"
+import { asyncHandler } from "../utility/asyncHandler"
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+})
+
+export {
+    healthcheck
+    }
+    
---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+import { asyncHandler } from "../utility/asyncHandler"
+
+
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+     const { channelId } = req.params
+     // TODO: toggle subscription
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+     const { channelId } = req.params
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+     const { subscriberId } = req.params
+})
+
+export {
+     toggleSubscription,
+     getUserChannelSubscribers,
+     getSubscribedChannels
+}
