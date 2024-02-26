0083ee3270ef391b42c3f166fa9c5c0779754f69
durgeshbachhav <bachhavdurgesh123@gmail.com>
2024-02-26 23:31:59 +0530

    adding subscription schema

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -1,21 +1,87 @@
 import mongoose from "mongoose"
 import { asyncHandler } from "../utility/asyncHandler"
-
+import { Subscribe } from "../models/subscription.model"
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
      const { channelId } = req.params
-     // TODO: toggle subscription
+     const userId = req.user?._id;
+
+     //check user is already subscribe a channel or not
+     try {
+          const existingSubscriber = await Subscribe.findOne({ subscriber: userId, channel: channelId })
+          if (existingSubscriber) {
+               await existingSubscriber.remove();
+               return res
+                    .status(200)
+                    .json(
+                         new Response(200, 'channel is unsubscribe')
+                    )
+          } else {
+               const newSubscription = await Subscribe.create({ subscriber: userId, channel: channelId });
+               return res
+                    .status(200)
+                    .json(
+                         new Response(200, newSubscription, 'channel subscribe successfully')
+                    )
+          }
+
+     } catch (error) {
+          throw new Response(500, error?.message || 'error while subsribing a channel')
+     }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
      const { channelId } = req.params
+
+     try {
+          const subscribers = await Subscribe.find({ channel: channelId }).populate('subscriber')
+
+          // Check if any subscribers were found
+          if (!subscribers || subscribers.length === 0) {
+               throw new ThrowError(404, 'No subscribers found for the channel');
+          }
+
+          const SubscriberDetail = subscribers.map(subscription => ({
+               subscriberId: subscription.subscriber.id,
+               subscriberName: subscription.subscriber.userName
+          }))
+
+          return res
+               .status(200)
+               .json(
+                    new Response(200, SubscriberDetail, 'subscriber detail are fetch successfully')
+               )
+     } catch (error) {
+          throw new ThrowError(500, error?.message || 'error while fetching subscribers')
+     }
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
      const { subscriberId } = req.params
+
+     try {
+          const channels = await Subscribe.find({ subscriber: subscriberId }).populate('channel')
+
+          if (!channels || channels.length === 0) {
+               throw new ThrowError(404, 'No subscribed channels found for the user');
+          }
+
+          const channelDetails = channels.map(subscription => ({
+               channelId: subscription.channel._id,
+               channelName: subscription.channel.channelName
+          }));
+
+          return res
+               .status(200)
+               .json(
+                    new Response(200, channelDetails, 'channellist fetch successfully')
+               )
+     } catch (error) {
+          throw new ThrowError(500, error.message || 'error while getting channelList')
+     }
 })
 
 export {
@@ -23,3 +89,4 @@ export {
      getUserChannelSubscribers,
      getSubscribedChannels
 }
+
